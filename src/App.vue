6c6a33de83719c730b3a1765db3896ce86374797
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-10-10 20:54:37 -0500

    category call

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
       <RouterView />
     </div>
   </main>
+  <p class="score">Score: 100</p>
 </template>
 
 <style lang="postcss" scoped>
@@ -14,5 +15,8 @@ main {
   & .card {
     @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
   }
+}
+.score {
+  @apply absolute bottom-4 right-2 text-2xl font-bold uppercase text-slate-800;
 }
 </style>
