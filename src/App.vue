6ab97348e736abcceab5c49a222dba7de1ea3c07
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-10-12 18:45:42 -0500

    refactor a little bit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
-<script setup></script>
+<script setup>
+import MainLayout from '@/components/MainLayout.vue'
+</script>
 
 <template>
-  <main>
-    <div class="card">
-      <RouterView />
-    </div>
-  </main>
-  <p class="score">Score: 100</p>
+  <MainLayout />
 </template>
-
-<style lang="postcss" scoped>
-main {
-  @apply flex h-screen items-center justify-center bg-gradient-to-r from-yellow-100 via-yellow-300 to-yellow-500;
-  & .card {
-    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
-  }
-}
-.score {
-  @apply absolute bottom-4 right-2 text-2xl font-bold uppercase text-slate-800;
-}
-</style>
---
+++ src/components/MainScore.vue
@@ -0,0 +1,9 @@
+<template>
+  <p class="score">Score: 100</p>
+</template>
+
+<style lang="postcss" scoped>
+.score {
+  @apply absolute bottom-4 right-2 text-2xl font-bold uppercase text-slate-800;
+}
+</style>
