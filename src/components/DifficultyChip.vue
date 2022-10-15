d1e2deb54a5de304931294a420314ade7d174ee3
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-10-14 20:03:55 -0500

    added difficulty

---
+++ src/components/DifficultyChip.vue
@@ -0,0 +1,29 @@
+<script setup>
+const props = defineProps({
+  difficulty: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <p
+    class="difficulty"
+    :class="
+      props.difficulty === 'easy'
+        ? 'bg-green-600'
+        : props.difficulty === 'medium'
+        ? 'bg-yellow-600'
+        : 'bg-red-500'
+    "
+  >
+    {{ props.difficulty }}
+  </p>
+</template>
+
+<style lang="postcss" scoped>
+.difficulty {
+  @apply absolute bottom-2 left-2 rounded-full bg-blue-400 px-4 py-1 text-sm font-bold capitalize text-slate-800;
+}
+</style>
