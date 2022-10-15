a2c32e1ea74af6521d7853916a4d32601aecfa55
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-10-14 20:03:44 -0500

    added scores

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,16 +1,27 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import { shuffle } from 'lodash-es'
+
 import useAPI from '@/composables/useAPI'
+import useScore from '@/composables/useScore'
 
 import BaseTitle from '@/components/BaseTitle.vue'
 import useColor from '@/composables/useColor'
+import DifficultyChip from '@/components/DifficultyChip.vue'
+import router from '@/router'
 
 const route = useRoute()
 const colors = useColor()
 const api = useAPI()
 const question = ref(null)
 const answers = ref([])
+const { changeScore } = useScore()
+
+const handleAnswer = (points) => {
+  changeScore(points)
+  router.push('/')
+}
 
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
@@ -18,14 +29,22 @@ onMounted(async () => {
     id: answers.value.length,
     correct: true,
     answer: question.value.correct_answer,
+    points:
+      question.value.difficulty === 'easy'
+        ? 10
+        : question.value.difficulty === 'medium'
+        ? 20
+        : 30,
   })
   question.value.incorrect_answers.map((answer) => {
     answers.value.push({
       id: answers.value.length,
       correct: false,
       answer,
+      points: -5,
     })
   })
+  answers.value = shuffle(answers.value)
 })
 </script>
 
@@ -39,10 +58,12 @@ onMounted(async () => {
         :key="answer.id"
         :class="colors.getColor(answer.id)"
         class="answer"
+        @click="handleAnswer(answer.points)"
       >
         {{ answer.answer }}
       </div>
     </div>
+    <DifficultyChip :difficulty="question.difficulty" />
   </div>
   <div v-else class="loading">Loading...</div>
 </template>
@@ -58,6 +79,9 @@ onMounted(async () => {
     @apply grid w-full flex-grow grid-cols-2  gap-8;
     & .answer {
       @apply flex min-w-full items-center justify-center text-4xl text-white;
+      &:hover {
+        @apply cursor-pointer;
+      }
     }
   }
 }
